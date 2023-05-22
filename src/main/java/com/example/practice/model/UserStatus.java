c7d281ee9ca5a0506b8462268e822f5fede82f0e
Hyeon-AM.OH <117336788+BasaltAm@users.noreply.github.com>
2023-05-22 18:43:19 +0900

    Initial commit

---
+++ src/main/java/com/example/practice/model/UserStatus.java
@@ -0,0 +1,5 @@
+package com.example.practice.model;
+
+public enum MemberStatus {
+    ACTIVE, DEACTIVATED
+}
---
+++ src/main/java/com/example/practice/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.practice.repository;
+
+import com.example.practice.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
